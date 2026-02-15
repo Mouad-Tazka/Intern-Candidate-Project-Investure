e408c971a8c0c93b0d383f0cc89cd6b18d0de147
Mouad Tazka <112440662+Mouad-Tazka@users.noreply.github.com>
2026-02-15 16:23:54 +0100

    Loaded excel file in

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const app = express();
 
+// loading excel file
+const XLSX = require("xlsx");
+const path = require("path");
+
+const filePath = path.join(__dirname, "data", "SoftwareInternAssignment.xlsx");
+
+const workbook = XLSX.readFile(filePath);
+
+
 // Health endpoint
 app.get("/health", (req, res) => {
   res.json({ ok: true });
