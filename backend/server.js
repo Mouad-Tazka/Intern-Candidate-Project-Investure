9a1544d59adc4755f5171644e8b0e3084c8ff290
Mouad Tazka <112440662+Mouad-Tazka@users.noreply.github.com>
2026-02-17 00:16:05 +0100

    changed formatDate function after confirming what excel was sending to me

--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,14 @@ const path = require("path");
 
 const filePath = path.join(__dirname, "data", "SoftwareInternAssignment.xlsx");
 
-const workbook = XLSX.readFile(filePath);
+const workbook = XLSX.readFile(filePath, {cellDates: true}); // "Telling Excel: “If it’s a date, give it to me as a real Date object.”"
 
 // excel rows to JSON
 const sheetName = "rawdata";
 const sheet = workbook.Sheets[sheetName];
 const rows = XLSX.utils.sheet_to_json(sheet);
+
+
 
 // extracting only what I need (referenceDate and dailyReturn)
 const cleanedData = rows.map(row => {
@@ -22,37 +24,23 @@ const cleanedData = rows.map(row => {
   };
 });
 
+
+
+
 // converting taken in date into clean universal format YYYY-MM-DD
 function formatDate(excelDate) {
-  // Case 1: Already a Date object
-  if (excelDate instanceof Date) {
-    return formatParts(
-      excelDate.getFullYear(),
-      excelDate.getMonth() + 1, // getMonth() in JavaScript is 0-11, +1 changes that to 1-12
-      excelDate.getDate()
-    );
-  }
-
-  // Case 2: Excel serial number
-  if (typeof excelDate === "number") {
-    const parsed = XLSX.SSF.parse_date_code(excelDate);
-    return formatParts(parsed.y, parsed.m, parsed.d);
-  }
-
-  // Case 3: String like "6/10/2020"
-  if (typeof excelDate === "string" && excelDate.includes("/")) {
-    const [month, day, year] = excelDate.split("/");
-    return formatParts(Number(year), Number(month), Number(day));
-  }
-
-  return null; 
+  if (!(excelDate instanceof Date)) return null;
+  return formatParts(
+    excelDate.getFullYear(),
+    excelDate.getMonth() + 1,
+    excelDate.getDate()
+  );
 }
-// formatting or case 3
-function formatParts(year, month, day) {
-// checks if 2 digits, if not, add 0 in front, i.e. -> "6" turns into "06", "11" stays as "11"
-  const mm = String(month).padStart(2, "0");
-  const dd = String(day).padStart(2, "0");
-  return `${year}-${mm}-${dd}`;
+
+function formatParts(year, month, day) { // checks if 2 digits, if not, add 0 in front, i.e. -> "6" turns into "06", "11" stays as "11"
+  const mm = String(month).padStart(2, "0"); 
+  const dd = String(day).padStart(2, "0"); 
+  return `${year}-${mm}-${dd}`; 
 }
 
 
